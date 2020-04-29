9417df4eac19afd35d62b1901ad30f38d4658fd6
Luís <koelho.luis@gmail.com>
2020-04-29 02:57:38 +0100

    gallery and camera works

--- app/src/main/java/pl/edu/lab3/i256991/MainActivity.java
+++ app/src/main/java/pl/edu/lab3/i256991/MainActivity.java
@@ -1,11 +1,14 @@
 package pl.edu.lab3.i256991;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,11 +25,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_STORAGE = 1 ;
+    static final int REQUEST_IMAGE_CAPTURE = 2;
+    private static final int MY_PERMISSIONS_REQUEST_READ_STORAGE = 10;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 20;
     private static int RESULT_LOAD_IMAGE = 1;
+
+    ImageView mImageView;
+    Uri img_uri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,33 +46,70 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //OLD SCHOOL IMAGE FROM GALLERY
+        mImageView = findViewById(R.id.imgView);
+        //OLD SCHOOL GET IMAGE FROM GALLERY
         Button buttonLoadImage = (Button) findViewById(R.id.uploadButton);
         buttonLoadImage.setOnClickListener(new View.OnClickListener() {
-
-            @Override
             public void onClick(View arg0) {
-
                 if (ContextCompat.checkSelfPermission(MainActivity.this,
                         Manifest.permission.READ_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
-
                     // Permission is not granted
-                        // No explanation needed; request the permission
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                                MY_PERMISSIONS_REQUEST_READ_STORAGE);
-
+                    // No explanation needed; request the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_READ_STORAGE);
                 } else {
-                    Intent i = new Intent(
-                            Intent.ACTION_PICK,
-                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                    startActivityForResult(i, RESULT_LOAD_IMAGE);
+                getImageFromGallery();
                 }
-
             }
         });
+
+        //OLD SCHOOL GET IMAGE FROM CAMERA
+        Button buttonTakePicture = (Button) findViewById(R.id.cameraButton);
+        buttonTakePicture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+             //check sys OS version
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    // system OS > marshmallow
+                    if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                                    PackageManager.PERMISSION_DENIED){
+                        //requesting permission
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        //show popup to request permissions
+                        requestPermissions(permission, MY_PERMISSIONS_REQUEST_CAMERA);
+                    }
+                    else {
+                        //permission already granted
+                        openCameraApp();
+                    }
+                }
+                else {
+                    //system OS < marshmallow
+                    openCameraApp();
+                }
+            }
+        });
+    }
+
+    private void getImageFromGallery() {
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
+    private void openCameraApp() {
+        ContentValues val = new ContentValues();
+        val.put(MediaStore.Images.Media.TITLE, "New Picture");
+        val.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+
+        img_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, val);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, img_uri);
+        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
     }
 
     @Override
@@ -69,24 +117,23 @@ public class MainActivity extends AppCompatActivity {
                                            String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_READ_STORAGE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    Intent i = new Intent(
-                            Intent.ACTION_PICK,
-                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                    startActivityForResult(i, RESULT_LOAD_IMAGE);
+                // READ GALLERY
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getImageFromGallery();
                 } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Please allow the permission...", Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // CAMERA
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                   openCameraApp();
+                } else {
+                    Toast.makeText(this, "Please allow the permission...", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
         }
     }
 
@@ -113,6 +160,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -129,8 +178,13 @@ public class MainActivity extends AppCompatActivity {
             String picturePath = cursor.getString(columnIndex);
             cursor.close();
 
-            ImageView imageView = (ImageView) findViewById(R.id.imgView);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            mImageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+        else
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK && null != data) {
+            mImageView.setImageURI(img_uri);
+
 
         }
     }
